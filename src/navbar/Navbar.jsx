5c0cb35f7910291abf58efee926af07d84746036
1-aminadab <4ethiochild@gmail.com>
2023-10-02 12:15:20 -0700

    nav bar menu

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,73 +1,105 @@
-import React, { useEffect, useState,useLayoutEffect } from 'react'
-import './Navbar.css'
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import "./Navbar.css";
 /// Toggle package
-import DayNightToggle from 'react-day-and-night-toggle'
+import DayNightToggle from "react-day-and-night-toggle";
 // ICONS
-import EmailIcon from '@mui/icons-material/Email';
-import TelegramIcon from '@mui/icons-material/Telegram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from "@mui/icons-material/Email";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-import MenuIcon from '@mui/icons-material/Menu';
-const navOptions =['home', 'projects', 'about', 'skill', 'contact']
+import MenuIcon from "@mui/icons-material/Menu";
+const navOptions = ["home", "projects", "about", "skill", "contact"];
 
 function Navbar() {
-  const [darkMode, setDarkMode] = useState(false)
-  const [clickedOption, setClickedOption] = useState(0)
-  const [openNav , setOpenNav] = useState(false)
-  useEffect(()=>{
-    handleDarkmode()
-  },[darkMode])
- 
-  const clickHandler = (id)=>{
-    setClickedOption(id)
-  }
-  const handleDarkmode = ()=>{
-    document.documentElement.style.setProperty('--dark-mode', `${darkMode ? '#2e2e2e':'#f6f6f6'}`)
-    document.documentElement.style.setProperty('--light-mode', `${darkMode ? '#ddd':'rgb(51, 51, 51)'}`)
-   }
+  const [darkMode, setDarkMode] = useState(false);
+  const [clickedOption, setClickedOption] = useState(0);
+  const [openNav, setOpenNav] = useState(false);
+  useEffect(() => {
+    handleDarkmode();
+  }, [darkMode]);
+
+  const clickHandler = (id) => {
+    setClickedOption(id);
+  };
+  const handleDarkmode = () => {
+    document.documentElement.style.setProperty(
+      "--dark-mode",
+      `${darkMode ? "#2e2e2e" : "#f6f6f6"}`
+    );
+    document.documentElement.style.setProperty(
+      "--light-mode",
+      `${darkMode ? "#ddd" : "rgb(51, 51, 51)"}`
+    );
+  };
 
   //  ////////////
-  
+
   //////////
   return (
-    <div className='nav-bar nav-blur'>
-      <div className='nav-menu' onClick={()=> setOpenNav(!openNav)}>
-      <MenuIcon fontSize='large'/>
+    <div className="nav-bar nav-blur">
+      <div className="nav-menu" onClick={() => setOpenNav(!openNav)}>
+        <MenuIcon fontSize="large" />
       </div>
-      <div className="nav-options" style={{animationName:`${openNav && 'nav-animation'}`}}>
-        <ul style = {{display:"flex",justifyContent:"space-between", gap:"10px"}}>
-          {
-            navOptions.map((option, id)=>{
-              return (
-                 <li onClick={()=>{
-                  clickHandler(id)
-                  setOpenNav(false)}} key={id} className={`${clickedOption === id && 'active'}`}><a href={`#${option}`}>{option.toUpperCase()}</a> </li>
-              )
-             
-            })
-          }
-          
-          
+      <div
+        className="nav-options"
+        style={{ animationName: `${openNav && "nav-animation"}` }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            gap: "10px",
+          }}
+        >
+          {navOptions.map((option, id) => {
+            return (
+              <li
+                onClick={() => {
+                  clickHandler(id);
+                  setOpenNav(false);
+                }}
+                key={id}
+                className={`${clickedOption === id && "active"}`}
+              >
+                <a href={`#${option}`}>{option.toUpperCase()}</a>{" "}
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="links">
         <ul>
-          <li><a href="https://mail.google.com/mail/u/developer.amanuel@gmail.com?"><EmailIcon /></a> </li>
-          <li><a href="https://www.linkedin.com/in/amanuel-tadesse-870416232"><LinkedInIcon /></a></li>
-          <li><a href="https://web.telegram.org"><TelegramIcon /></a></li>
-          <li><a href="https://github.com/1-aminadab"><GitHubIcon /></a></li>
+          <li>
+            <a href="https://mail.google.com/mail/u/developer.amanuel@gmail.com?">
+              <EmailIcon />
+            </a>{" "}
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/amanuel-tadesse-870416232">
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://web.telegram.org">
+              <TelegramIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/1-aminadab">
+              <GitHubIcon />
+            </a>
+          </li>
         </ul>
       </div>
-      <div className='day-night-toggle'>
-          <DayNightToggle
-      onChange={() => setDarkMode(!darkMode)}
-      checked={darkMode}
-    />
+      <div className="day-night-toggle">
+        <DayNightToggle
+          onChange={() => setDarkMode(!darkMode)}
+          checked={darkMode}
+        />
       </div>
-    
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
