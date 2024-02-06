63eaefe20435339f5dc8233b5f501c04efd62554
Amanuel Tadesse <developer.amanuel@gmail.com>
2024-02-06 12:15:08 +0300

    here we go

--- src/contents/about/About.jsx
+++ src/contents/about/About.jsx
@@ -8,7 +8,7 @@ function About() {
        Hi, I'm Amanuel, a seasoned full stack developer with over 2.5 years of professional experience. I possess a strong expertise in crafting visually stunning and highly functional web applications. My skill set encompasses both front-end and back-end development, allowing me to seamlessly navigate through various stages of the development lifecycle.
       </p>
       <p>
-      I have a degree in Computer Engineering and I have gained valuable experience and honed my skills through freelancing for various clients. Working independently has allowed me to work on diverse projects and collaborate with clients from different industries. These experiences have provided me with a deep understanding of client requirements and the ability to deliver high-quality solutions within deadlines.
+      I have a degree in Computer Engineering and I have gained valuable experience and honed my skills through freelancing for various clients as well as employee. Working independently has allowed me to work on diverse projects and collaborate with clients from different industries. These experiences have provided me with a deep understanding of client requirements and the ability to deliver high-quality solutions within deadlines.
       </p>
       <p>
         I am always excited to take on new challenges and contribute to innovative projects. When I'm not coding, you can find me exploring the outdoors, reading tech blogs, or attending meetups and conferences to expand my professional network.
