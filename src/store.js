8a867cf40c126f1b340045cc79c6badd3aea7858
1-aminadab <4ethiochild@gmail.com>
2024-06-18 18:23:56 +0300

    convert to next js

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import darkMode from './features/ld_mode/mode'
-export const store = configureStore({
-  reducer: {
-    darkMode:darkMode
-  },
-})
